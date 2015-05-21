35ec7655ee8615a494cbc358431b83cf54ed0c57
jylock <jsuo.mail@gmail.com>
2015-05-21 01:41:02 -0700

    Happiness Example

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,58 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Junyuan Suo on 4/10/15.
+//  Copyright (c) 2015 JYLock. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController, FaceViewDataSource
+{
+    var happiness: Int = 25 { // 0 = very sad, 100 = ecstatic
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            println("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+    }
+    
+    
+    
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+        }
+    }
+    
+    
+    func updateUI()
+    {
+        faceView.setNeedsDisplay()
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness - 50)/50
+    }
+    
+}
